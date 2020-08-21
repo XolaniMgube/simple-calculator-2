09018322347058e3070f14dc3a523eeae0d4327d
XolaniMgube <xnmgube@gmail.com>
2020-08-21 08:22:48 +0200

    added describe blocks for each test

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,37 +1,58 @@
 "use strict"
 
-function idk() {
-    
-}
-
-describe("Testing the Calculator class", function () {
-
+function unrandomizeTesting() {
     beforeEach(function () {
         global.Calculator = require("../src/calculator.js")
         global.calculatorInstance = new Calculator()
     });
+}
+
+describe("Testing the Calculator class for the add function", function () {
+
+    unrandomizeTesting()
 
     it("returns the sum of add function", function () {
         expect(calculatorInstance.add(1, 2)).toBe(3)
         expect(calculatorInstance.add(1, 2, 3, 4)).toBe(10)
     })
+})
+
+
+describe("Testing the Calculator class for the multiply function", function () {
+
+    unrandomizeTesting()
 
     it("returns the product of the multiply function", function () {
         expect(calculatorInstance.multiply(1, 2)).toBe(2)
         expect(calculatorInstance.multiply(1, 2, 3, 4)).toBe(24)
     })
+})
+
+describe("Testing for the storage of the last function", function () {
+
+    unrandomizeTesting()
 
     it("gets the last element in the storage array", function () {
         calculatorInstance.add(1, 2);
         calculatorInstance.multiply(1, 2)
         expect(calculatorInstance.last()).toBe(2)
     })
+})
+
+describe("Testing for passing arguments as strings", function () {
+
+    unrandomizeTesting()
 
     it("allows the last function to be passed as a string argument", function () {
         calculatorInstance.add(1, 2)
         calculatorInstance.multiply(1, 2);
         expect(calculatorInstance.multiply("LAST", 5)).toBe(10)
     })
+})
+
+describe("Testing for storage of indexes", function () {
+
+    unrandomizeTesting()
 
     it("returns an element on a specific index determined by the setSlot argument", function () {
         calculatorInstance.add(1, 2)
@@ -47,8 +68,8 @@ describe("Testing the Calculator class", function () {
         expect(calculatorInstance.getSlot(2)).toBe(30)
         expect(calculatorInstance.last()).toBe(300)
 
-        expect(calculatorInstance.add(calculatorInstance.last(), 10)).toBe(310)
-        expect(calculatorInstance.add(calculatorInstance.getSlot(1), 5)).toBe(8)
+        expect(calculatorInstance.add("LAST", 10)).toBe(310)
+        expect(calculatorInstance.add("SLOT_1", 5)).toBe(8)
         expect(calculatorInstance.multiply("SLOT_2", 2)).toBe(60)
     })
 })
